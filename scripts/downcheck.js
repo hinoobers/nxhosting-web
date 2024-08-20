ea81fefe466faa2c1472b403e92efc6a320ba465
reiop <redisolenmina1@gmail.com>
2024-08-20 09:19:26 +0300

    Switch to tailwind

--- scripts/downcheck.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function() {
-    this.fetchAPI("status", "GET")
-        .then(response => {
-            console.log(response);
-        })
-});
--- scripts/shared.js
+++
@@ -1,8 +0,0 @@
-const API_URL = "http://server2.hinoob.xyz:5080/status";
-
-function fetchAPI(endpoint, method) {
-    console.log("test");
-    return fetch(`${API_URL}${endpoint}`, { method })
-        .then(response => response.json())
-        .catch(error => console.error(error));
-}
