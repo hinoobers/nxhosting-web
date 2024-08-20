33e76ffb95a3c7009b5a6e3a55211806ec65ff8b
reiop <redisolenmina1@gmail.com>
2024-08-20 04:19:45 +0300

    Add sample click detection to top bar btns

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    this.document.getElementById("login-btn").addEventListener("click", function() {
+        console.log("Login button clicked");
+    });
+    this.document.getElementById("signup-btn").addEventListener("click", function() {
+        console.log("Register button clicked");
+    });
+});
